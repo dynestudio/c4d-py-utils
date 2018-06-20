3e3eab89262869632f3c774a008fb37ca82dda5c
Carlos Dordelly <cdordelly09@gmail.com>
2018-06-20 00:18:26 -0400

    correct code structure

--- develop/obj_select all objects.py
+++ develop/obj_select all objects.py
@@ -1,14 +1,16 @@
 import c4d
- 
-def GetNextObject(op): # object manager iteration
-    if not op: return None
-    if op.GetDown(): return op.GetDown()
-    while not op.GetNext() and op.GetUp(): op = op.GetUp()
-    return op.GetNext()
- 
-def main():
+  
+def get_allObjs():
+    def GetNextObject(op): # object manager iteration
+        if not op: return None
+        if op.GetDown(): return op.GetDown()
+        while not op.GetNext() and op.GetUp(): op = op.GetUp()
+        return op.GetNext()
+
     # get first obj
     first_obj = doc.GetFirstObject()
+    if not first_obj:
+        return None
     # list of all objects in the scene
     list_objs = []
     # add the first obj
@@ -19,9 +21,8 @@ def main():
         first_obj = GetNextObject(first_obj)
         if first_obj:
             list_objs.append(first_obj)
-        
-    # update the scene
-    c4d.EventAdd()
+
+    return list_objs
 
 if __name__=='__main__':
-    main()
+    get_allObjs()
