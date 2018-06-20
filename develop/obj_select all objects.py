c37a04a5ccd20044126b53ffbab2ef144825b16f
Carlos Dordelly <cdordelly09@gmail.com>
2018-06-20 00:12:57 -0400

    working base code

--- develop/obj_select all objects.py
+++ develop/obj_select all objects.py
@@ -1,32 +1,27 @@
 import c4d
  
 def GetNextObject(op): # object manager iteration
-    if not op:
-        return None
- 
-    if op.GetDown():
-        return op.GetDown()
- 
-    while not op.GetNext() and op.GetUp():
-        op = op.GetUp()
- 
+    if not op: return None
+    if op.GetDown(): return op.GetDown()
+    while not op.GetNext() and op.GetUp(): op = op.GetUp()
     return op.GetNext()
  
 def main():
     # get first obj
-    frist_obj = doc.GetFirstObject()
+    first_obj = doc.GetFirstObject()
     # list of all objects in the scene
     list_objs = []
     # add the first obj
-    list_objs.append(frist_obj)
+    list_objs.append(first_obj) 
 
     # obj loop iteration
-    while frist_obj:          
-        obj = GetNextObject(frist_obj)
-        list_objs.append(obj)
+    while first_obj:          
+        first_obj = GetNextObject(first_obj)
+        if first_obj:
+            list_objs.append(first_obj)
         
-    print len(list_objs)
-    print list_objs
+    # update the scene
+    c4d.EventAdd()
 
 if __name__=='__main__':
     main()
